38b221944e99284f8ef5293402729d0bbc230784
bryancitu <bryanjesus.ra@gmail.com>
2021-10-12 00:34:17 -0500

    button led 4

--- button_led.py
+++ button_led.py
@@ -7,24 +7,24 @@ GPIO.cleanup()
 
 # GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(17, GPIO.IN)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
+GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 def encender_led(pin):
-    GPIO.output(6, GPIO.HIGH)
+    GPIO.output(15, GPIO.HIGH)
     print("Interrupcion")
 
 def apagar_led(pin):
-    GPIO.output(6, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW)
 
 GPIO.add_event_detect(17, GPIO.RISING , callback = encender_led)
 # GPIO.add_event_detect(4,GPIO.RISING, callback = apagar_led)
 
 while 1:
-    GPIO.output(5, GPIO.HIGH)
-    time.sleep(5)
-    GPIO.output(5, GPIO.LOW)
-    time.sleep(5)
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(14)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(14)
     # if GPIO.input(4):
     #     GPIO.output(6, GPIO.HIGH)
     # else:
