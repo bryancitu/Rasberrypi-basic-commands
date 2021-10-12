7c720295094e7b95c9785d47bc1ee784715ca613
bryancitu <bryanjesus.ra@gmail.com>
2021-10-11 23:41:24 -0500

    button led 1

---
+++ button_led.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# Entradas
+
+# GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+
+def encender_led(pin):
+    GPIO.output(6, GPIO.HIGH)
+
+def apagar_led(pin):
+    GPIO.output(6, GPIO.LOW)
+
+GPIO.add_event_detect(4,GPIO.RISING, callback = encender_led)
+# GPIO.add_event_detect(4,GPIO.RISING, callback = apagar_led)
+
+while 1:
+    GPIO.output(5, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(5, GPIO.LOW)
+    time.sleep(5)
+    # if GPIO.input(4):
+    #     GPIO.output(6, GPIO.HIGH)
+    # else:
+    #     GPIO.output(6,GPIO.LOW)
